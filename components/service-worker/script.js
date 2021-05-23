2642cc85f6348e2063d377ee2509190de9205f23
Volodymyr S <73670163+volvalder@users.noreply.github.com>
2021-05-23 16:24:30 -0700

    Update script.js

--- components/service-worker/script.js
+++ components/service-worker/script.js
@@ -1,10 +1,13 @@
 if('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('./sw.js', {scope: './'}).then(() => {
-		if (navigator.serviceWorker.controller) {
-			console.info('this page is being controlled by ' + navigator.serviceWorker.controller);
-			const img = document.createElement('img');
-			img.setAttribute('src', 'dwnld.svg');
-			document.body.appendChild(img);
-		}
-	});
+    navigator.serviceWorker.register('./sw.js', {scope: './'}).then(() => {
+        if (navigator.serviceWorker.controller) {
+            console.info('this page is being controlled by ' + navigator.serviceWorker.controller);
+        }
+    });
 }
+
+setTimeout(() => {
+    const img = document.createElement('img');
+    img.setAttribute('src', 'dwnld.svg');
+    document.body.appendChild(img);
+}, 2000);
