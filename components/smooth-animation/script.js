fa5b2f3762c66920b49f1a3e35785d745dc1f0bb
Neurmayer <volvalder@gmail.com>
2021-06-24 17:07:31 -0700

    implementing animation comparising.

--- components/smooth-animation/script.js
+++ components/smooth-animation/script.js
@@ -1,4 +1,7 @@
-const element = document.getElementById('container');
+const element = document.getElementById('container1');
+const element2 = document.getElementById('container2');
+const element3 = document.getElementById('container3');
+const element4 = document.getElementById('container4');
 
 function moveLeft(el, duration, distance) {
     const start = performance.now();
@@ -18,4 +21,36 @@ function moveLeft(el, duration, distance) {
     requestAnimationFrame(move);
 }
 
-moveLeft(element, 3000, 1500);
+function simpleMoveLeft(el, duration, distance) {
+    const start = performance.now();
+
+    function move(curTime) {
+        const elapsed = curTime - start;
+        // this gets an equal parts of total duration
+        const progress = elapsed / duration;
+        const bitToMove = progress * distance;
+
+        el.style.left = `${bitToMove}px`;
+        if (bitToMove < distance) {
+            requestAnimationFrame(move);
+        }
+    }
+
+    requestAnimationFrame(move);
+}
+
+function intervalMoveLeft(el, duration, distance) {
+    let x = 0;
+    setInterval(() => {
+        el.style.left = `${x}px`;
+        // why 10 instead of 16 ? cause browser will take 6ms to draw a frame
+        x += distance / duration * 10;
+    }, 10);
+
+}
+
+moveLeft(element, 10000, 1500);
+simpleMoveLeft(element2, 10000, 1500);
+//to start css animation
+setTimeout(() => {element3.classList.add('go')}, 0);
+intervalMoveLeft(element4, 10000, 1500);
