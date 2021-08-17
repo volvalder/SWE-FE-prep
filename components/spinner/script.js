cf5ba53b51afe94a0176d7f3ed757103505842e9
Volodymyr S <73670163+volvalder@users.noreply.github.com>
2021-08-17 13:03:29 -0700

    Create script.js

---
+++ components/spinner/script.js
@@ -0,0 +1,28 @@
+const btn = document.getElementById('button');
+const spinner = document.getElementById('spinner');
+
+let cycles = 0;
+
+btn.addEventListener('click', () => {
+	if(spinner.classList.contains('animating')) {
+  	cycles ++;
+    return;
+  }
+  action();
+});
+
+spinner.addEventListener('animationend', () => {
+	spinner.style.animation = '';
+	spinner.classList.remove('animating');
+  if(cycles > 0) {
+  	cycles --;
+    action();
+  }
+});
+
+function action() {
+  setTimeout(() => {
+    spinner.classList.add('animating');
+    spinner.style.animation = 'spin 3s linear';
+  }, 0);
+}
