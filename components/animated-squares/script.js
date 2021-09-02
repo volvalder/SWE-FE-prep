b2d4bd5ab35ba275ae8ee6f0cfabb0360ac376f4
Volodymyr S <73670163+volvalder@users.noreply.github.com>
2021-09-01 18:08:36 -0700

    Update script.js

--- components/animated-squares/script.js
+++ components/animated-squares/script.js
@@ -1,64 +1,62 @@
 customElements.define('animated-squares', class extends HTMLElement {
-  busy = false;
+    busy = false;
 
-  constructor() {
-    super();
-    this.stack = [];
-  }
-  
-  connectedCallback() {
-    const [x, y] = this.getAttribute('dimensions').split('-');
-    this.size = x*y;
-    this.buildField(+x, +y);
-    this.addListener();
-  }
-  
-  buildField(x, y) {
-    const wrapper = document.createDocumentFragment();
-    const table = document.createElement('div');
-    table.classList.add('table');
-    
-    for(let i = 0; i < x; i++) {
-      const row = document.createElement('div');
-      row.classList.add('row');
-
-      for(let j = 0; j < y; j++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        row.appendChild(cell);
-      }
-
-      table.appendChild(row);
+    constructor() {
+        super();
+        this.stack = [];
     }
-    wrapper.appendChild(table);
 
-    this.appendChild(wrapper);
-  }
-  
-  addListener() {
-    const els = this.querySelectorAll('.cell');
-    els.forEach((el) => {
-      el.addEventListener('click', () => {
-        if(this.busy) return;
-          this.stack.push(el);
-          el.classList.add('hidden');
-          if(this.stack.length === this.size) {
-            this.busy = true;
-            this.bringBack();
+    connectedCallback() {
+        const [x, y] = this.getAttribute('dimensions').split('-');
+        this.size = x*y;
+        this.buildField(+x, +y);
+        this.addListener();
+    }
+
+    buildField(x, y) {
+        const wrapper = document.createDocumentFragment();
+        const table = document.createElement('div');
+        table.classList.add('table');
+
+        for(let i = 0; i < x; i++) {
+            const row = document.createElement('div');
+            row.classList.add('row');
+
+            for(let j = 0; j < y; j++) {
+                const cell = document.createElement('div');
+                cell.classList.add('cell');
+                row.appendChild(cell);
+            }
+            table.appendChild(row);
         }
-      });
-    });
-  }
-  
-  bringBack() {
-    if(!this.stack.length) {
-      this.busy = false;
-      return;
+    wrapper.appendChild(table);
+    this.appendChild(wrapper);
     }
-    const el = this.stack.pop();
-    setTimeout(() => {
-      el.classList.remove('hidden');
-      this.bringBack();
-    }, 1000);
-  }
+  
+    addListener() {
+        const els = this.querySelectorAll('.cell');
+        els.forEach((el) => {
+            el.addEventListener('click', () => {
+                if(this.busy) return;
+                this.stack.push(el);
+                el.classList.add('hidden');
+                if(this.stack.length === this.size) {
+                    this.busy = true;
+                    this.bringBack();
+                }
+            });
+        });
+    }
+
+    bringBack() {
+        if(!this.stack.length) {
+            this.busy = false;
+            return;
+        }
+        const el = this.stack.pop();
+        setTimeout(() => {
+            el.classList.remove('hidden');
+            this.bringBack();
+        }, 1000);
+    }
 });
