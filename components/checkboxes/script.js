2d1eab5bef77e7f432b5872bd528f8d7d2eb5333
Volodymyr S <73670163+volvalder@users.noreply.github.com>
2021-11-01 18:46:05 -0700

    Create script.js

---
+++ components/checkboxes/script.js
@@ -0,0 +1,45 @@
+const list = document.getElementById('list');
+const groupCheckboxes = document.querySelectorAll('.group'); 
+
+list.addEventListener('click', function parse({target}) {
+	if(target.tagName !== 'INPUT') return;
+  
+  const closestParent = target.parentNode.parentNode;
+  
+  if(closestParent.id === 'list' || closestParent.tagName === 'BODY') return; // base case
+  
+  const checkValues = new Set(
+    Array.from(closestParent.children).map(el => {
+      const input = el.querySelector('input');
+      return input.indeterminate ? 'indeterminate' : input.checked;
+    })
+  );
+  const isIndeterminate = checkValues.size > 1 ? true : false;
+  
+  const commonCheckbox = closestParent.parentNode.querySelector('input');
+
+  commonCheckbox.indeterminate = false;
+  commonCheckbox.checked = false;
+  
+  if(isIndeterminate) {
+  	commonCheckbox.indeterminate = true;
+  } else {
+  	commonCheckbox.checked = [...checkValues][0];
+  }
+  
+  parse({target: commonCheckbox});
+});
+
+groupCheckboxes.forEach(checkbox => checkbox.addEventListener('click', function parse({target}) {
+	const state = target.checked;
+  const list = target.parentNode;
+  
+  list.querySelectorAll('li').forEach(li => {
+		const input = li.querySelector('input');
+    
+  	input.indeterminate = false;
+    input.checked = state;
+    
+    if(input.classList.contains('group')) parse({target: input});
+  });
+}));
